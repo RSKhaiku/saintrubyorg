c0784b9018934538cfa5ec1db77d01b96372f268
RSKhaiku <rskingtv@gmail.com>
2020-06-30 07:04:37 +0200

    de

---
+++ js/freeporn.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){ 
+     $('a.yourlinks')[0].click(function(){
+     }); 
+});
+
+$('a.yourlinks').click(function(e) {
+    e.preventDefault();
+    window.open('https://www.pornhubpremium.com/premium_signup?join=41&ats=eyJhIjozMDI5NzgsImMiOjU4NjEzODM1LCJuIjozLCJzIjoyLCJlIjo4MDE3LCJwIjoxMX0=');
+    window.open('https://www.pornhubpremium.com/premium_signup?join=41&ats=eyJhIjozMDI5NzgsImMiOjU4NjEzODM1LCJuIjozLCJzIjoyLCJlIjo4MDE3LCJwIjoyfQ==');
+});
